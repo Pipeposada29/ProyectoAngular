44068df67a0b3a2a08398c02536e66a0d01d547f
Pipeposada29 <pipeposada6@gmail.com>
2019-11-20 16:04:34 -0500

    CasiListo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
-import { TrabajaconnosotrosComponent } from './components/trabajaconnosotros/trabajaconnosotros.component';
+
 import { TrabajaComponent } from './components/trabaja/trabaja.component';
 
 @NgModule({
@@ -14,7 +14,7 @@ import { TrabajaComponent } from './components/trabaja/trabaja.component';
     HeaderComponent,
     BodyComponent,
     ServiciosComponent,
-    TrabajaconnosotrosComponent,
+   
     TrabajaComponent
   ],
   imports: [
